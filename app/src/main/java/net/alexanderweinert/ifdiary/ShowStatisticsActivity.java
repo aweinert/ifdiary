af02354ae4f5ce9b24f597e8d2beaa661825727d
Alexander Weinert <alexander.weinert@gmx.net>
2019-09-29 19:19:58 +0200

    Add menu bar and settings icon

--- app/src/main/java/net/alexanderweinert/ifdiary/ShowStatisticsActivity.java
+++ app/src/main/java/net/alexanderweinert/ifdiary/ShowStatisticsActivity.java
@@ -8,11 +8,14 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.os.Handler;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import com.github.mikephil.charting.charts.PieChart;
@@ -157,6 +160,13 @@ public class ShowStatisticsActivity extends AppCompatActivity {
         chart.invalidate();
 
         chart.setCenterText(String.format("Fastenquote %d Tage", dateRangeSize));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
